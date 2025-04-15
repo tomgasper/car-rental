97f0f78f071ab8612caf5a2ec60c1439840166be
TG <tomgasper01@gmail.com>
2025-04-15 13:47:18 +0200

    Configure database connection and controllers

--- src/Infrastructure/AppDbContext.cs
+++ src/Infrastructure/AppDbContext.cs
@@ -1,7 +1,11 @@
+using CarRental.src.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarRental.src.Infrastructure;
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) {}
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+    DbSet<Car> Cars { get;set;}
+    DbSet<Reservation> Reservations { get;set; }
 }
