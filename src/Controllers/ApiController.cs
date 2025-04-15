160d67c2be6f88c8d693eb18a6bab876586ae06a
TG <tomgasper01@gmail.com>
2025-04-15 17:46:50 +0200

    Add global system for dealing with errors

--- src/Controllers/ApiController.cs
+++ src/Controllers/ApiController.cs
@@ -1,7 +1,57 @@
+using CarRental.src.Common;
+using FluentResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 [ApiController]
 class ApiController : ControllerBase
 {
-    
+    protected IActionResult Problem(List<IError> errors)
+        {
+        if (errors.Count == 0)
+        {
+            return Problem();
+        }
+
+        if (errors.All(error => error is ValidationError))
+        {
+            return ValidationProblem(errors.Cast<ValidationError>().ToList());
+        }
+
+        HttpContext.Items[HttpContextItemKeys.Errors] = errors;
+        
+        var error = errors[0];
+        if (error is IApplicationError applicationError)
+        {
+            return ApplicationProblem(applicationError);
+        }
+
+            return Problem();
+    }
+
+    private IActionResult ValidationProblem(List<ValidationError> errors)
+        {   
+        var modelStateDictionary = new ModelStateDictionary();
+        foreach (var error in errors)
+        {
+            modelStateDictionary.AddModelError(
+                error.PropertyName,
+                error.Message);
+        }
+        return ValidationProblem(modelStateDictionary);
+    }
+
+    private IActionResult ApplicationProblem(IApplicationError applicationError)
+    {
+        var statusCode = applicationError.ErrorType switch
+        {
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            ErrorType.Forbidden => StatusCodes.Status403Forbidden,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        return Problem(statusCode: statusCode, title: applicationError.Message);
+    }
 }
